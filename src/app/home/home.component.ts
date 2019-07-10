384d40667b1813cb7f2f780a15f5814dc781b143
Chema Muñoz <chemunoz78@gmail.com>
2019-07-10 21:24:33 +0200

    Reseting start day for the countdown

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -37,7 +37,7 @@ export class HomeComponent implements OnInit {
 
   countdown = () => {
     // Set the date we're counting down to
-    let countDownDate = new Date("Aug 17, 2019 20:15:00").getTime();
+    let countDownDate = new Date("Aug 16, 2019 20:15:00").getTime();
 
     // Update the count down every 1 second
     let x = setInterval(function() {
