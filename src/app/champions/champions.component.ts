6813c78b28d33d7ee210ea6d9a476d4f2912e9e4
Chema Muñoz <chemunoz78@gmail.com>
2019-07-21 17:33:46 +0200

    Refactored all countdowns

--- src/app/champions/champions.component.ts
+++ src/app/champions/champions.component.ts
@@ -9,49 +9,45 @@ export class ChampionsComponent implements OnInit {
 
   constructor() {
     const Champions = [
-      // SORTEO
-      {
-        deadline: new Date('Dec 21, 2019 21:00:00').getTime(),
-        date: { days: 0, hours: 0, minutes: 0, seconds: 0 },
-        distance: 0,
-        text: '',
-        container: 'countdown-draw-Champions'
+      { // SORTEO
+        deadline: 'Dec 21, 2019 21:00:00',
+        element: 'countdown-draw-Champions'
       },
-      // COMIENZO
-      {
-        deadline: new Date('Jan 05, 2020 17:00:00').getTime(),
-        date: { days: 0, hours: 0, minutes: 0, seconds: 0 },
-        distance: 0,
-        text: '',
-        container: 'countdown-Champions'
+      { // COMIENZO
+        deadline: 'Jan 05, 2020 17:00:00',
+        element: 'countdown-Champions'
       }
     ];
 
     const Competitions = [Champions[0], Champions[1]];
 
     // Update the count down every 1 second
-    setInterval(() => {
+    let x = setInterval(() => {
       // Get todays date and time
       const now = new Date().getTime();
 
-      Competitions.forEach((competition)=>{
+      Competitions.length === 0 ? clearInterval(x) : null;
+      Competitions.forEach((competition, index)=>{
         // Find the distance between now an the count down date
-        competition.distance = competition.deadline - now;
-        if (competition.distance < 0) {
+        let countDownDate = new Date(competition.deadline).getTime();
+        let distance = countDownDate - now;
+        let text = '';
+
+        if (distance < 0) {
           // If the count down is over, write some text
-          competition.text = 'COMENZADA!!';
-          clearInterval();
+          text = 'COMENZADA!!';
+          Competitions.splice(index, 1);
         } else {
           // Time calculations for days, hours, minutes and seconds
-          competition.date.days = Math.floor(competition.distance / (1000 * 60 * 60 * 24));
-          competition.date.hours = Math.floor((competition.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          competition.date.minutes = Math.floor((competition.distance % (1000 * 60 * 60)) / (1000 * 60));
-          competition.date.seconds = Math.floor((competition.distance % (1000 * 60)) / 1000);
-          competition.text = `${competition.date.days}d ${competition.date.hours}h 
-                              ${competition.date.minutes}m ${competition.date.seconds}s`;
-          // Output the result validating that the element exists
-          document.getElementById(competition.container) !== null ? document.getElementById(competition.container).innerHTML = competition.text : null;
+          let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          text = `${days}d ${hours}h ${minutes}m ${seconds}s`;
         }
+
+        // Output the result validating that the element exists
+        document.getElementById(competition.element) !== null ? document.getElementById(competition.element).innerHTML = text : null;
       });
     }, 1000);
   }
