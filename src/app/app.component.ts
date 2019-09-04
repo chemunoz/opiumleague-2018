1b13d82fb7d75a8128911a69dc8bc817799407b1
Chema Muñoz <josemmgarc@ext.zara.com>
2019-09-04 08:59:43 +0200

    Updated Google Analytics for loggin in SPA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter  } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Opium League';
+
+  constructor(private router:Router){
+    const navEndsEvents$ = this.router.events.pipe(filter(event => event instanceof NavigationEnd));
+    navEndsEvents$.subscribe((event:NavigationEnd) => {
+      gtag('config', 'UA-92241042-3', { 'page_path': event.urlAfterRedirects});
+    });
+  }
 }
