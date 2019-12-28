6619a80717d05dd88d39699d8317ceba180fcebd
Chema Muñoz <chemunoz78@gmail.com>
2019-12-28 18:51:26 +0100

    CHAMPIONS: Updated draw groups and datetime

--- src/app/champions/champions.component.ts
+++ src/app/champions/champions.component.ts
@@ -11,7 +11,7 @@ export class ChampionsComponent implements OnInit {
   constructor() {
     this.Champions = [
       { // SORTEO
-        deadline: 'Dec 21, 2019 21:00:00',
+        deadline: 'Dec 28, 2019 22:00:00',
         element: 'countdown-draw-Champions',
         distance: 0
       },
